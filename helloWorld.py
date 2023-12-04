03e01a26859bef42c0bdc6019b4efd5454b7ce3f
ShutaoWu <152294481+ShutaoWu@users.noreply.github.com>
2023-12-04 13:35:51 +0100

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,32 @@
+#print("Hello, World")
+
+#print("Python has three numeric types: int, float and complex")
+
+# myValue = 1
+# print(str(myValue) + " is of the data type" + str(type(myValue)))
+
+# myValue = 3.14
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# myValue = 5j
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# myValue= True
+# print(myValue)
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+name = input("What is your name? ")
+print(name)
+
+color = input("What is your favourite color? ")
+animal = input("What is your favourite animal? ")
+print("{}, you like a {} {}!".format(name,color,animal))
